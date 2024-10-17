4a32b0c29553f07087cbe826a77a8794a5edbd8d
sippin <regulationqq@mail.ru>
2024-10-17 20:35:40 +0300

    fixed links

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -8,9 +8,15 @@ export default function Header() {
       <header className='header flex justify-between p-5 bg-selfgray  border border-selfborder rounded-lg mt-10 mb-10'>
         <span className='text-selfpurple'>VELIKANOVA DASHA</span>
         <nav className='header_items flex'>
-          <li className='header_item mr-10 cursor-pointer hover:text-selfpurple'><Link href="#skills" />НАВЫКИ</li>
-          <li className='header_item mr-10 cursor-pointer hover:text-selfpurple'><Link href="#project" />ПРОЕКТЫ</li>
-          <li className='header_item mr-10 cursor-pointer hover:text-selfpurple' ><Link href="#contact" />КОНТАКТЫ</li>
+          <li className='header_item mr-10 cursor-pointer hover:text-selfpurple'>
+            <Link href="#skills">НАВЫКИ</Link>
+          </li>
+          <li className='header_item mr-10 cursor-pointer hover:text-selfpurple'>
+            <Link href="#project">ПРОЕКТЫ</Link>
+          </li>
+          <li className='header_item mr-10 cursor-pointer hover:text-selfpurple'>
+            <Link href="#contact">КОНТАКТЫ</Link>
+          </li>
         </nav>
       </header>
 
