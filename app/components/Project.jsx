a574f76408a94d9d548fa35f4681a5e55530526f
sippin <regulationqq@mail.ru>
2025-03-13 18:28:08 +0300

    edit proj

--- app/components/Project.jsx
+++ app/components/Project.jsx
@@ -9,25 +9,26 @@ export default function Project() {
 
     const projects = [
         {
-            title: 'IThub | Справки и Админ',
+            title: 'IThub | Справки и Admin',
 
             role: 'Разработчик ',
-            details: 'IThub | Справки – сервис для онлайн-заказа и отслеживания справок студентами, что избавляет от необходимости посещать администрацию лично. \n IThub | Admin – централизованная база данных для администраторов, упрощающая управление студентами и поддерживающая актуальность информации.',
+            details: 'IThub | Справки – сервис для онлайн-заказа и отслеживания справок студентами. \n IThub | Admin – централизованная система управления студентами для администраторов.',
             image: ithub
         },
-
         {
-            title: 'Сайт для фермерского хозяйства',
+            title: 'Лендинг для фермерского хозяйства',
             role: 'Разработчик',
-            details: 'Визитная карта фермерского хозяйства с характеристиками урожая и административной панелью для обновления данных параметров урожая ежегодно',
+            details: 'Визитная карта фермера для продажи своего урожая',
             image: farming
         },
+
         {
             title: 'Haste',
             role: 'Дизайнер',
             details: 'Дизайн для мобильного приложения онлайн-коворкинга',
             image: haste
-        }
+        },
+
     ];
 
     return (
@@ -42,7 +43,7 @@ export default function Project() {
                         <div key={index} className='project_item flex mb-8 border rounded-lg bg-selfgray border-selfborder '>
                             {el.image && <Image className='project_image rounded-lg shrink-0' src={el.image} alt='project image' />}
                             <div className='project_info flex flex-col p-5'>
-                                <h1 className='mb-2'><span className='text-selfpurple'>Проект:</span> <span className='project_title font-bold'>{el.title}</span> </h1>
+                                <div className='mb-2'><span className='text-selfpurple'>Проект:</span> <span className='project_title font-bold'>{el.title}</span> </div>
                                 <span className='project_role mb-2'><span className='text-selfpurple'>Роль:</span> {el.role}</span>
                                 <span className='project_dis'>{el.details}</span>
                             </div>
